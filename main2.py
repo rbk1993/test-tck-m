3e7d08e550c55a649a1327e42ba71584953cb4ed
rbk1993 <reda.benkirane@hotmail.fr>
2025-08-09 18:34:37 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -11,7 +11,7 @@ URL = "https://www.cafonline.com/caf-africa-cup-of-nations/news/"
 KEYWORDS = [
     "ticket", "tickets", "sale", "buy", "purchase",
     "available", "book", "ticketing", "admission", "access",
-    "launch"
+    "launch", "highli"
 ]
 
 # Clés et identifiants
